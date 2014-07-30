a214d5df0ed87555807a7f626555401c3ee399f6
Eugenio F <eug.alfe@gmail.com>
2014-07-30 18:23:54 -0300

    added support to remove multiple paths

--- vanish.py
+++ vanish.py
@@ -7,72 +7,85 @@ from glob import glob
 from os import remove, listdir
 from os.path import isfile, join, expandvars, expanduser
 
+
 def is_valid_json(json):
-	""" Checks if the json file is correct """
+  """ Checks if the json file is correct """
 
-	isvalid = True
+  isvalid = True
 
-	for program, value in json.items():
+  for program, value in json.items():
 
-		if not program:
-			print("[ERROR] Missing program name for: " + value)
+    if not program:
+      print("[ERROR] Missing program name for: " + value)
 
-		for name in value:
-			
-			if not 'active' in value[name]:
-				isvalid = False
-				print("[ERROR] Missing 'active' attribute for '" + name + "'")
+    for name in value:
+      
+      if not 'active' in value[name]:
+        isvalid = False
+        print("[ERROR] Missing 'active' attribute for '" + name + "'")
 
-			if not 'path' in value[name]:
-				isvalid = False
-				print("[ERROR] Missing 'path' attribute for '" + name + "'")
+      if not 'path' in value[name]:
+        isvalid = False
+        print("[ERROR] Missing 'path' attribute for '" + name + "'")
 
-			if not 'description' in value[name]:
-				print("[WARNING] Missing 'description' attribute for '" + name + "'")
+      if not 'description' in value[name]:
+        print("[WARNING] Missing 'description' attribute for '" + name + "'")
 
-	return isvalid
+  return isvalid
+
+
+def expand(path):
+  return expandvars(expanduser(path))
 
 
 def rm_content(path):
-	""" Remove file or direcotry content """
+  """ Remove file or direcotry content """
 
-	if isfile(path):
-		remove(path)
-	else:
-		for f in listdir(path):
+  if isfile(path):
+    remove(path)
+  else:
+    for f in listdir(path):
 
-			file_path = join(path, f)
+      file_path = join(path, f)
 
-			if isfile(file_path):
-				remove(file_path)
-			else:
-				rmtree(file_path)
+      if isfile(file_path):
+        remove(file_path)
+      else:
+        rmtree(file_path)
 
 
 def do_clean(json):
-	""" Traverse through json file removing files/directories """
+  """ Traverse through json file removing files/directories """
 
-	for program, value in json.items():
-		
-		for name in value:
+  for program, option in json.items():
+    
+    for name in option:
 
-			active = value[name]['active']
-			path   = value[name]['path']
-			path   = expanduser(path)
-			path   = expandvars(path)
+      active = option[name]['active']
+      paths  = option[name]['path']
 
-			if not active: continue
+      if isinstance(paths, list):
+        paths = [expand(path) for path in paths]
+      else:
+        paths = [expand(paths)]
 
-			for f in glob(path):
-				print("Removing '" + f + "'")
-				rm_content(f)
+      if not active: continue
+
+      for path in paths:
+        for f in glob(path):
+          print("Removing '" + f + "'")
+          # rm_content(f)
 
 
 if __name__ == '__main__':
-	
-	with open("default.json") as default:
-		
-		json = json.load(default)
 
-		if is_valid_json(json):
-			do_clean(json)
+  cleaners_dir = 'cleaners'
+
+  for cleaner in listdir(cleaners_dir):
+
+    with open(join(cleaners_dir, cleaner)) as f:
+
+      data = json.load(f)
+
+      if is_valid_json(data):
+        do_clean(data)
