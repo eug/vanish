69d6d92cfe5193e8f8bbc76edbf7e398f9f468f5
Eugenio F <eug.alfe@gmail.com>
2014-07-31 23:37:51 -0300

    pep8 formated

--- vanish.py
+++ vanish.py
@@ -1,7 +1,8 @@
-#!/bin/env python
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 import json
+import logging
 from shutil import rmtree
 from glob import glob
 from os import remove, listdir
@@ -9,83 +10,81 @@ from os.path import isfile, join, expandvars, expanduser
 
 
 def is_valid_json(json):
-  """ Checks if the json file is correct """
+    """ Checks if the json file is correct """
 
-  isvalid = True
+    isvalid = True
 
-  for program, option in json.items():
+    for program, option in json.items():
 
-    if not program:
-      print("[ERROR] Missing program name for: " + option)
+        if not program:
+            print("[ERROR] Missing program name for: " + option)
 
-    for name in option:
-      
-      if not 'active' in option[name]:
-        isvalid = False
-        print("[ERROR] Missing 'active' attribute for '" + name + "'")
+        for name in option:
 
-      if not 'path' in option[name]:
-        isvalid = False
-        print("[ERROR] Missing 'path' attribute for '" + name + "'")
+            if not 'active' in option[name]:
+                isvalid = False
+                print("[ERROR] Missing 'active' attribute for '" + name + "'")
 
-      if not 'description' in option[name]:
-        print("[WARNING] Missing 'description' attribute for '" + name + "'")
+            if not 'path' in option[name]:
+                isvalid = False
+                print("[ERROR] Missing 'path' attribute for '" + name + "'")
 
-  return isvalid
+            if not 'description' in option[name]:
+                print("[WARNING] Missing 'description' attribute for '" + name + "'")
+
+    return isvalid
 
 
 def expand(path):
-  return expandvars(expanduser(path))
+    return expandvars(expanduser(path))
 
 
 def rm_content(path):
-  """ Remove file or direcotry content """
+    """ Remove file or direcotry content """
 
-  if isfile(path):
-    remove(path)
-  else:
-    for f in listdir(path):
+    if isfile(path):
+        remove(path)
+    else:
+        for f in listdir(path):
 
-      file_path = join(path, f)
+            file_path = join(path, f)
 
-      if isfile(file_path):
-        remove(file_path)
-      else:
-        rmtree(file_path)
+            if isfile(file_path):
+                remove(file_path)
+            else:
+                rmtree(file_path)
 
 
 def do_clean(json):
-  """ Traverse through json file removing files/directories """
+    """ Traverse through json file removing files/directories """
 
-  for program, option in json.items():
-    
-    for name in option:
+    for program, option in json.items():
 
-      active = option[name]['active']
-      paths  = option[name]['path']
+        for name in option:
 
-      if isinstance(paths, list):
-        paths = [expand(path) for path in paths]
-      else:
-        paths = [expand(paths)]
+            active = option[name]['active']
+            paths = option[name]['path']
 
-      if not active: continue
+            if not active:
+                continue
 
-      for path in paths:
-        for f in glob(path):
-          print("Removing '" + f + "'")
-          rm_content(f)
+            if isinstance(paths, list):
+                paths = [expand(path) for path in paths]
+            else:
+                paths = [expand(paths)]
+
+            for path in paths:
+                for f in glob(path):
+                    print("Removing '" + f + "'")
+                    rm_content(f)
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
 
-  cleaners_dir = 'cleaners'
+    cleaners_dir = 'cleaners'
 
-  for cleaner in listdir(cleaners_dir):
-
-    with open(join(cleaners_dir, cleaner)) as f:
-
-      data = json.load(f)
-
-      if is_valid_json(data):
-        do_clean(data)
+    for cleaner in listdir(cleaners_dir):
+        with open(join(cleaners_dir, cleaner)) as f:
+            data = json.load(f)
+            if is_valid_json(data):
+                do_clean(data)
